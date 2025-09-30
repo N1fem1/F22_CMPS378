a76e9a0f31fff4074623bc937894cd258fdaa488
N1fem1 <anthonyadeeyinwo@gmail.com>
2025-09-29 20:19:18 -0700

    Add files via upload

---
+++ FallSchedule/FallSchedule/src/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+	std::cout << "===========================================================================" << "\n";
+	std::cout << " | | | | | |      My Fall Schedule           " <<  "\n";
+	std::cout << "===========================================================================" << "\n\n\n";
+
+	std::cout << "| Day    | Time        | Course Code   | Course Title            |Location       | Instructor          |" << std::endl;
+	std::cout << "------------------------------------------------------------------------" << "\n";
+	std::cout << "|MW      |7:40 - 9:15  |  CS 218       | Statistics              | Abraham campus | Prof. Kittiyaratch |" << std::endl;
+	std::cout << "|MW      |9:25 - 10:15 |  Kine 001     | Fitness for live        | Abraham Campus | Prof. Allison      |" << std::endl;
+	std::cout << "|TT      |10:20 - 11:55|  CS 218       | Programming Concepts    | Founders Hall  | Prof. Juan         |" << std::endl;
+	std::cout << "|TT      |12:05 - 1:40 |  CS 218       | Publishing on the Web I | Founders Hall  | Prof. Jozef        |" << std::endl;
+	std::cout << "|TT      |1:50 - 3:25  |  RCS 110      | College Writing A       | Founders Hall  | Prof. Kristine     |" << "\n\n\n";
+	
+	std::cout << "------------------------------------------------------------------------" << "\n\n\n";
+
+	std::cout << "Total Units:  12" << "\n\n\n";
+
+	std::cout << "Make sure to check your Student Portal weekly for schedule updates!" << "\n";
+
+	std::cout << "===========================================================================" << "\n";
+	return 0;
+}
