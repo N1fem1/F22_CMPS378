a09ad83ab60889d5d9ce12decfa6c69e609abca8
N1fem1 <anthonyadeeyinwo@gmail.com>
2025-09-29 20:26:36 -0700

    Add files via upload

---
+++ Aboutme/About Me Profile/About Me Profile/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	std::cout << "===========================WHO AM I? - PERSONAL PROFILE ================================================" << "\n\n";
+	
+	
+	std::cout << "Full Name:       Oluwanifemi Adeeyinwo" << "\n";
+	std::cout << "Nickname:        Nifemi" << "\n";
+	std::cout << "Age:             17" << "\n";
+	std::cout << "Height:          5.9 feet" << "\n";
+	std::cout << "Favorite Color:  Forest Green     " << "\n";
+	std::cout << "City of Birth:   Nigeria " << "\n";
+	std::cout << "Favorite Food:   White Rice and Chicken" << "\n";
+	std::cout << "Number of Pets:  0" << "\n";
+	std::cout << "Favorite Anime:  Black Clover" << "\n";
+	std::cout << "Car Right Now:   R/T Challenger " << "\n";
+	std::cout << "Dream Car:       C8 Z06 Corvette " << "\n\n\n";
+
+	std::cout << "-------------------------------------------------------------------------------------------------------" << "\n";
+
+	std::cout << "Nice to meet you! My name is Oluwanifemi but most people call me Nifemi.I'm 17 years old and currently dreaming of owning a C8 Z06 Corvette as my second car/dream car." << std::endl;
+	return 0;
+}
